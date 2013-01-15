6ce1c1ff4ee8a4d6811424e90375fb0312081f0c
cka3b <cka3b@yandex.com>
2013-01-14 17:42:21 -0800

    Create RU.py

---
+++ RU.py
@@ -0,0 +1,194 @@
+# RU.py #
+# Russian cycrillic constants
+
+Capital_Letter_Io = u'\u0401'
+Capital_Letter_A = u'\u0410'  
+Capital_Letter_Be = u'\u0411'	
+Capital_Letter_Ve = u'\u0412'	
+Capital_Letter_Ghe = u'\u0413'	
+Capital_Letter_De = u'\u0414'	
+Capital_Letter_Ie = u'\u0415'	
+Capital_Letter_Zhe = u'\u0416'	
+Capital_Letter_Ze = u'\u0417'	
+Capital_Letter_I = u'\u0418'	
+Capital_Letter_Short_I = u'\u0419'	
+Capital_Letter_Ka = u'\u041A'	
+Capital_Letter_El = u'\u041B'	
+Capital_Letter_Em = u'\u041C'	
+Capital_Letter_En = u'\u041D'	
+Capital_Letter_O = u'\u041E'	
+Capital_Letter_Pe = u'\u041F'	
+Capital_Letter_Er = u'\u0420'	
+Capital_Letter_Es = u'\u0421'	
+Capital_Letter_Te = u'\u0422'	
+Capital_Letter_U = u'\u0423'	
+Capital_Letter_Ef = u'\u0424'	
+Capital_Letter_Ha = u'\u0425'
+Capital_Letter_Tse = u'\u0426'
+Capital_Letter_Che = u'\u0427'	
+Capital_Letter_Sha = u'\u0428'
+Capital_Letter_Shcha = u'\u0429'	
+Capital_Letter_Hard_Sign = u'\u042A'	
+Capital_Letter_Yeru = u'\u042B'	
+Capital_Letter_Soft_Sign = u'\u042C'	
+Capital_Letter_E = u'\u042D'	
+Capital_Letter_Yu = u'\u042E'	
+Capital_Letter_Ya = u'\u042F'	
+Small_Letter_A = u'\u0430'	
+Small_Letter_Be = u'\u0431'	
+Small_Letter_Ve = u'\u0432'	
+Small_Letter_Ghe = u'\u0433'	
+Small_Letter_De = u'\u0434'	
+Small_Letter_Ie = u'\u0435'	
+Small_Letter_Zhe = u'\u0436'	
+Small_Letter_Ze = u'\u0437'	
+Small_Letter_I = u'\u0438'	
+Small_Letter_Short_I = u'\u0439'	
+Small_Letter_Ka = u'\u043A'	
+Small_Letter_El = u'\u043B'	
+Small_Letter_Em = u'\u043C'	
+Small_Letter_En = u'\u043D'	
+Small_Letter_O = u'\u043E'	
+Small_Letter_Pe = u'\u043F'	
+Small_Letter_Er = u'\u0440'	
+Small_Letter_Es = u'\u0441'	
+Small_Letter_Te = u'\u0442'	
+Small_Letter_U = u'\u0443'	
+Small_Letter_Ef = u'\u0444'	
+Small_Letter_Ha = u'\u0445'	
+Small_Letter_Tse = u'\u0446'	
+Small_Letter_Che = u'\u0447'	
+Small_Letter_Sha = u'\u0448'	
+Small_Letter_Shcha = u'\u0449'	
+Small_Letter_Hard_Sign = u'\u044A'	
+Small_Letter_Yeru = u'\u044B'	
+Small_Letter_Soft_Sign = u'\u044C'	
+Small_Letter_E = u'\u044D'	
+Small_Letter_Yu = u'\u044E'	
+Small_Letter_Ya = u'\u044F'	
+Small_Letter_Io = u'\u0451'	
+
+abc_capital = [
+	Capital_Letter_Io,
+	Capital_Letter_A,	
+	Capital_Letter_Be,	
+	Capital_Letter_Ve,	
+	Capital_Letter_Ghe,	
+	Capital_Letter_De,	
+	Capital_Letter_Ie,	
+	Capital_Letter_Zhe,	
+	Capital_Letter_Ze,	
+	Capital_Letter_I,	
+	Capital_Letter_Short_I,	
+	Capital_Letter_Ka,	
+	Capital_Letter_El,	
+	Capital_Letter_Em,	
+	Capital_Letter_En,	
+	Capital_Letter_O,	
+	Capital_Letter_Pe,	
+	Capital_Letter_Er,	
+	Capital_Letter_Es,	
+	Capital_Letter_Te,	
+	Capital_Letter_U,	
+	Capital_Letter_Ef,	
+	Capital_Letter_Ha,
+	Capital_Letter_Tse,
+	Capital_Letter_Che,	
+	Capital_Letter_Sha,
+	Capital_Letter_Shcha,	
+	Capital_Letter_Hard_Sign,	
+	Capital_Letter_Yeru,	
+	Capital_Letter_Soft_Sign,	
+	Capital_Letter_E,	
+	Capital_Letter_Yu,	
+	Capital_Letter_Ya
+]
+
+abc_small = [
+	Small_Letter_Io,
+	Small_Letter_A,	
+	Small_Letter_Be,	
+	Small_Letter_Ve,	
+	Small_Letter_Ghe,	
+	Small_Letter_De,	
+	Small_Letter_Ie,	
+	Small_Letter_Zhe,	
+	Small_Letter_Ze,	
+	Small_Letter_I,	
+	Small_Letter_Short_I,	
+	Small_Letter_Ka,	
+	Small_Letter_El,	
+	Small_Letter_Em,	
+	Small_Letter_En,	
+	Small_Letter_O,	
+	Small_Letter_Pe,	
+	Small_Letter_Er,	
+	Small_Letter_Es,	
+	Small_Letter_Te,	
+	Small_Letter_U,	
+	Small_Letter_Ef,	
+	Small_Letter_Ha,
+	Small_Letter_Tse,
+	Small_Letter_Che,	
+	Small_Letter_Sha,
+	Small_Letter_Shcha,	
+	Small_Letter_Hard_Sign,	
+	Small_Letter_Yeru,	
+	Small_Letter_Soft_Sign,	
+	Small_Letter_E,	
+	Small_Letter_Yu,	
+	Small_Letter_Ya
+]
+
+
+WOVELS = [
+	Capital_Letter_Io     ,Small_Letter_Io,
+	Capital_Letter_A      ,Small_Letter_A,
+	Capital_Letter_Ie     ,Small_Letter_Ie,
+	Capital_Letter_I      ,Small_Letter_I,
+	Capital_Letter_O      ,Small_Letter_O,
+	Capital_Letter_U      ,Small_Letter_U,
+	Capital_Letter_E      ,Small_Letter_E,
+	Capital_Letter_Yu     ,Small_Letter_Yu,
+	Capital_Letter_Ya     ,Small_Letter_Ya
+	]
+
+CONSONANTS = [
+	Small_Letter_Be,	   Capital_Letter_Be,	
+	Small_Letter_Ve,	   Capital_Letter_Ve,	
+	Small_Letter_Ghe,	   Capital_Letter_Ghe,	
+	Small_Letter_De,	   Capital_Letter_De,	
+	Small_Letter_Ie,	   Capital_Letter_Ie,	
+	Small_Letter_Zhe,	   Capital_Letter_Zhe,	
+	Small_Letter_Ze,	   Capital_Letter_Ze,	
+	Small_Letter_Short_I,	Capital_Letter_Short_I,
+	Small_Letter_Ka,	   Capital_Letter_Ka,	
+	Small_Letter_El,	   Capital_Letter_El,	
+	Small_Letter_Em,	   Capital_Letter_Em,	
+	Small_Letter_En,	   Capital_Letter_En,	
+	Small_Letter_Pe,	   Capital_Letter_Pe,	
+	Small_Letter_Er,	   Capital_Letter_Er,	
+	Small_Letter_Es,	   Capital_Letter_Es,	
+	Small_Letter_Te,	   Capital_Letter_Te,	
+	Small_Letter_Ef,	   Capital_Letter_Ef,	
+	Small_Letter_Ha,       Capital_Letter_Ha,
+	Small_Letter_Tse,      Capital_Letter_Tse,
+	Small_Letter_Che,	   Capital_Letter_Che,	
+	Small_Letter_Sha,      Capital_Letter_Sha,
+	Small_Letter_Shcha,	   Capital_Letter_Shcha,	
+]
+
+
+SIGNS = [
+	Capital_Letter_Hard_Sign,	Small_Letter_Hard_Sign,	
+	Capital_Letter_Yeru,	    Small_Letter_Yeru,	
+	Capital_Letter_Soft_Sign,	Small_Letter_Soft_Sign ]
+
+PUNCTUATION = [',','.']
+SOFTENING = [
+	Capital_Letter_Io     ,Small_Letter_Io,
+	Capital_Letter_Ie     ,Small_Letter_Ie,
+	Capital_Letter_I      ,Small_Letter_I,
+	Capital_Letter_Yu     ,Small_Letter_Yu,
+	Capital_Letter_Ya     ,Small_Letter_Ya,
+	Capital_Letter_Soft_Sign,	Small_Letter_Soft_Sign ]
